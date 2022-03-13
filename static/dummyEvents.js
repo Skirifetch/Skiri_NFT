06d4e45a549e3ed0f6b022f9c11e367184169293
Hamza MARHOUN <marhounh@fp-mc-411966.home>
2022-03-13 19:07:11 +0100

    NFT celeb

---
+++ static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+  {
+    price: 17,
+    from: '0xLouis-Marie',
+    to: '0xVincent',
+    date: '4 months ago',
+  },
+  {
+    price: 15,
+    from: '0xHamza',
+    to: '0xLouis-Marie',
+    date: '5 months ago',
+  },
+  {
+    price: 13,
+    from: '0xJohn M',
+    to: '0xDavid',
+    date: '7 months ago',
+  },
+  {
+    price: 13,
+    from: '0xDeltatre',
+    to: '0xDavid',
+    date: '7 months ago',
+  },
+]
